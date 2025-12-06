a05e92fe631acd69e09a4f13e3c5439b2983ee7e
Wenor <kirillmogel@gmail.com>
2025-12-06 17:01:33 +0300

    Add files via upload

---
+++ cs2-dumper-main/output/panorama_dll.hpp
@@ -0,0 +1,20 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-12-06 13:57:39.411044100 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: panorama.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace panorama_dll {
+            // Parent: None
+            // Field count: 0
+            namespace ELayoutNodeType {
+            }
+        }
+    }
+}
